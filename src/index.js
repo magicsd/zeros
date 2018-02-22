73dca8631f4af26605252a48c0d8084bc3fba358
Alex <sd@alexdus.com>
2018-02-22 04:00:03 +0300

    4 утра, но я это сделал

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  if (number >= 0 && number < 5) return 0;
+  if (number > 0) {
+
+  let count = 0;
+  for (let i = 1; Math.floor(number / Math.pow(5, i)) >= 1; i++) {
+    count += Math.floor(number / Math.pow(5, i));
+  }
+
+  return count;
+  }
 }
